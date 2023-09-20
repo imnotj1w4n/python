29e477deaa8facb1914f21a6eaf0c370ebe4fde2
j1w4n <133107610+j1w4n@users.noreply.github.com>
2023-09-20 18:43:20 +0900

    Add files via upload

---
+++ 거북ㄱ이가 도장을 찍으면서 원으 ㄹ그려요.py
@@ -0,0 +1,18 @@
+import turtle
+
+t=turtle.Turtle()
+t.shape("turtle")
+t.circle(90,90)
+st1=t.stamp()
+t.circle(90,90)
+st2=t.stamp()
+t.circle(90,90)
+st3=t.stamp()
+t.circle(90,90)
+
+st4=t.stamp()
+t.clearstamp(st1)
+t.clearstamp(st3)
+
+##turtle.done()
+##
