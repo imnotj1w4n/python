7f99636d613075f952ecdbd06972d6b80c6c8b03
j1w4n <133107610+j1w4n@users.noreply.github.com>
2023-11-15 18:38:21 +0900

    Add files via upload

---
+++ 멋진 문자 여러번 출력하기.py
@@ -0,0 +1,4 @@
+n = input("문자 입력")
+for i in range(0,5):
+    if i < 5:
+        print("%s"%n ,end = '')
---
+++ 짝수를 ㅈ찾아용.py
@@ -0,0 +1,4 @@
+n = int(input("1-100  까지 정수중 하나를 입력하에ㅐㅇ뇽:"))
+for i in range(n):
+    if i % 2 == 0:
+        print("%d"%(i))
